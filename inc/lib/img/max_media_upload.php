ad598de31d793205bf000864b008ba7ec50c9334
Sjoerd Takken <info@sjoerdscomputerwelten.de>
2020-10-15 20:30:34 +0200

    Bug fix

--- inc/lib/img/max_media_upload.php
+++ inc/lib/img/max_media_upload.php
@@ -1,7 +1,46 @@
 <?php
 
-add_filter('wp_handle_upload_prefilter', 
-           'max_media_upload_error_message');
+add_filter( 'upload_size_limit', 
+            'wplib_filter_site_upload_size_limit', 20 );
+
+/**
+ * Filter the upload size limit for non-administrators.
+ *
+ * @param string $size Upload size limit (in bytes).
+ * @return int (maybe) Filtered size limit.
+ */
+function wplib_filter_site_upload_size_limit( $size ) 
+{
+  // Set the upload size limit to 10 MB for users lacking the 'manage_options' capability.
+  if ( current_user_can( 'manage_options' ) ) 
+  {
+    return $size;
+  }
+
+  $enabled = get_option('wplib_max_media_uploadenabled');
+  if(empty($enabled))
+  {
+    return $size;
+  }
+
+  if(!$enabled)
+  {
+    return $size;
+  }
+
+  $limit = get_option('wplib_max_media_uploadsize');
+  if(empty($limit))
+  {
+    $limit = 500;
+  }
+ 
+  return 1024 * $limit;
+}
+
+
+
+//add_filter('wp_handle_upload_prefilter', 
+//           'max_media_upload_error_message');
 
 function max_media_upload_error_message($file) 
 {
@@ -30,7 +69,7 @@ function max_media_upload_error_message($file)
   return $file;
 }
 
-add_action('admin_head', 'upload_load_styles');
+//add_action('admin_head', 'upload_load_styles');
 
 function upload_load_styles() 
 {
